3479cf44e1fa8e2862db3e63c541380f68bd82e8
Sly <sylwesterbartnicki@gmail.com>
2020-04-16 20:53:44 +0200

    HTTP class lesson

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function sayit(name) {
-  console.log(`Hello ${name}`)
-}
+const http = require('http')
 
-sayit('dude')
+const server = http.createServer(function (req, res) {
+  if (req.url === '/') {
+    res.write('Hello world')
+    res.end()
+  }
+  if (req.url === '/api/courses') {
+    res.write('courses here')
+    res.end()
+  }
+})
+
+server.listen(3000)
+
+console.log('listening on port 3000')
